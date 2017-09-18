55c4f9e629ca690192211de651bb7f95024e8728
Unknown <anthony.giraudo@etu.univ-amu.fr>
2017-09-18 10:03:02 +0200

    definition de la premiere fonction

--- astro.py
+++ astro.py
@@ -7,3 +7,9 @@ from re import * #pour les expressions régulières
 fo = open("data_modifié.txt", 'r')
 data = fo.read()
 fo.close()
+
+def lire_fichier(fichier):
+    """
+    :param fichier: nom du fichier en chaine de caractere. Le fichier est trié par colonnes
+    :return: retourne listes qui correspondent chacune à une colonne du fichier
+    """
