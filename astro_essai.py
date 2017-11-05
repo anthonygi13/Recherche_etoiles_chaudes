d62ee81987f33cff192d8af801b6a8f6c7dc04de
Unknown <anthony.giraudo@etu.univ-amu.fr>
2017-11-05 15:30:58 +0100

    essai reecriture des fonctions pour un code plus logique, et plus souple d'utilisation

--- astro_essai.py
+++ astro_essai.py
@@ -1,11 +1,9 @@
-# S3 Projet 2017
-# Licence MPCI
 # Authors:   Bratulic Melanie, mail: m.bratulic@laposte.net
 #            Durand-Viel Albane, mail:
 #            Giraudo Anthony, mail: anthonygi13@hotmail.fr
 #            Marinho Louise, mail : louise.marinho@free.fr
-# Creation date : 10/20/2017
-# File : astro_v4.py
+# Creation date : 11/04/2017
+# File : astro_essai.py
 
 from pylab import *
 import os
@@ -49,8 +47,11 @@ def main_sequence(g_r):
     return None
 
 
-def lines(filename, n_c1, n_c2):
+def lines(filename, n_c1, n_c2, column_separator, begining_str, comentary_char):
     """
+    comentary_char c au debut d une ligne ou ca y est pas
+    column_separator c un seul caractere
+    begining str c la ligne juste avant le debut
     :param filename: name of file, where the data of the stars are. We want the values of c1 and c2
     :param n_c1: number of column c1 in our file
     :param n_c2: number of column c1 in our file
@@ -60,56 +61,59 @@ def lines(filename, n_c1, n_c2):
     data = open(filename, 'r')
     line = data.readline()
 
-    while line[0:2] != "--": # the columns start after a line of '-----'
-        line = data.readline()
+    if begining_str is not None and begining_str != "":
+        while line[0:len(begining_str)] != begining_str:
+            line = data.readline()
 
     line = data.readline()
 
     while line != "":
-        c1 = ""
-        c2 = ""
-        n_colonne = 1
-        for char in line:
-            if char == "|":
-                n_colonne += 1
-            if n_colonne == n_c1:
-                if char != " " and char != "|":
-                    c1 += char
-            elif n_colonne == n_c2:
-                if char != " " and char != "|":
-                    c2 += char
-            if n_colonne > max([n_c1, n_c2]):
-                break
-        if c1 == "":
-            c1 = None
-        if c2 == "":
-            c2 = None
-        yield c1, c2
+
+        if comentary_char != None and comentary_char != "" and line[0] != comentary_char:
+            c1 = ""
+            c2 = ""
+            column_number = 1
+            for char in line:
+                if char == column_separator:
+                    column_number += 1
+                if column_number == n_c1:
+                    if char != " " and char != column_separator:
+                        c1 += char
+                elif column_number == n_c2:
+                    if char != " " and char != column_separator:
+                        c2 += char
+                if column_number > max([n_c1, n_c2]):
+                    break
+            if c1 == "":
+                c1 = None
+            if c2 == "":
+                c2 = None
+            yield c1, c2
+
         line = data.readline()
 
     data.close()
 
 
-def get_magnitudes(filename, n_g_r, n_u_g):
+def get_magnitudes(filename, n_g_r, n_u_g, column_separator, begining_str, comentary_char):
     """
-    Mélanie
     :param filename: name of the file (txt format) containing a table with the data of the scars
     :param n_g_r: index of the column containing the (g-r) characteristic in the file filename
     :param n_u_g: index of the column containing the (u-g) characteristic in the file filename
     :return: 2 lists : the first one containing the data of the (g-r) column in the file filename, and the other one the (u-g) column
     """
 
-    colonne_u_g = []
-    colonne_g_r = []
-    for g_r, u_g in lines(filename, n_g_r, n_u_g): #for each line of the file filename, takes the characteristic (g-r) and (u-g)
-        if u_g is not None: colonne_u_g.append(float(u_g))
-        else: colonne_u_g.append(u_g)
-        if g_r is not None: colonne_g_r.append(float(g_r))
-        else: colonne_g_r.append(g_r)
-    return colonne_g_r, colonne_u_g
+    u_g_column = []
+    g_r_column = []
+    for g_r, u_g in lines(filename, n_g_r, n_u_g, column_separator, begining_str, comentary_char): #for each line of the file filename, takes the characteristic (g-r) and (u-g)
+        if u_g is not None: u_g_column.append(float(u_g))
+        else: u_g_column.append(u_g)
+        if g_r is not None: g_r_column.append(float(g_r))
+        else: g_r_column.append(g_r)
+    return g_r_column, u_g_column
 
 
-def find_hot_stars(input_file, output_file, output_folder=None, n_g_r=6, n_u_g=5):
+def find_hot_stars(input_file, output_file, n_g_r, n_u_g, column_separator, begining_str, comentary_char, output_folder=None):
     """
     :param input_file: input file with lines of data separated by
     :param output_file: nom du fichier qui contiendra les donnees correspondant uniquement aux etoiles chaudes
@@ -138,13 +142,12 @@ def find_hot_stars(input_file, output_file, output_folder=None, n_g_r=6, n_u_g=5
     data = open(input_file, 'r')
     nfile = open(output_file, "w")
 
-    nfile.write("HOT STARS\n")
-
     line = data.readline()
 
-    while line[0:2] != "--":
-        nfile.write(line)
-        line = data.readline()
+    if begining_str is not None and begining_str != "":
+        while line[0:len(begining_str)] != begining_str:
+            nfile.write(line)
+            line = data.readline()
 
     nfile.write(line)
     line = data.readline()
@@ -152,32 +155,39 @@ def find_hot_stars(input_file, output_file, output_folder=None, n_g_r=6, n_u_g=5
     i = 0
 
     while line != "":
+
         i += 1
         if i % 10000 == 0:
-            print("avancement : ", i)
-        u_g = ""
-        g_r = ""
-        n_colonne = 1
-        for char in line:
-            if char == "|":
-                n_colonne += 1
-            if n_colonne == n_u_g:
-                if char != " " and char != "|":
-                    u_g += char
-            elif n_colonne == n_g_r:
-                if char != " " and char != "|":
-                    g_r += char
-            if n_colonne > max([n_u_g, n_g_r]):
-                break
-        if u_g != "" and g_r != "" and float(u_g) <= B3V_line(float(g_r)):
+            print(i, " lines already read")
+
+        if comentary_char is not None and comentary_char != "" and line[0] != comentary_char:
+            u_g = ""
+            g_r = ""
+            column_number = 1
+            for char in line:
+                if char == column_separator:
+                    column_number += 1
+                if column_number == n_u_g:
+                    if char != " " and char != column_separator:
+                        u_g += char
+                elif column_number == n_g_r:
+                    if char != " " and char != column_separator:
+                        g_r += char
+                if column_number > max([n_u_g, n_g_r]):
+                    break
+            if u_g != "" and g_r != "" and float(u_g) <= B3V_line(float(g_r)):
+                nfile.write(line)
+
+        if comentary_char is not None and comentary_char != "" and line[0] == comentary_char:
             nfile.write(line)
+
         line = data.readline()
 
     data.close()
     nfile.close()
 
 
-def fichier_reg(input_file, output_file, output_folder=None, n_alpha=3, n_delta=4):
+def fichier_reg(input_file, output_file, n_alpha, n_delta, column_separator, begining_str, comentary_char, output_folder=None, circle_size=5, circle_color="green"):
     """
     moi
     :param input_file: fichier avec les etoiles chaudes
@@ -208,20 +218,20 @@ def fichier_reg(input_file, output_file, output_folder=None, n_alpha=3, n_delta=
 
     nfile.write('# Region file format: DS9 version 4.1\n')
     nfile.write(
-        'global color=green dashlist=8 3 width=1 font=\"helvetica 10 normal roman\" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1\n')
+        'global color=' + circle_color + 'dashlist=8 3 width=1 font=\"helvetica 10 normal roman\" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1\n')
     nfile.write('fk5')
 
-    for alpha, delta in lines(input_file, n_alpha, n_delta):
+    for alpha, delta in lines(input_file, n_alpha, n_delta, column_separator, begining_str, comentary_char):
         nfile.write("\n")
-        nfile.write('circle(' + alpha + ',' + delta + ',5\")')
+        nfile.write('circle(' + alpha + ',' + delta + ',' + str(circle_size) + '\")')
 
     nfile.close()
 
 
-def plot_graphic(title, data_filename, SP_filename="SP.txt", n_g_r_data=6, n_u_g_data=5, n_g_r_SP=4, n_u_g_SP=3,
-                    hot_stars_filename=None):
+def plot_u_g_vs_g_r(title, filename, n_g_r, n_u_g, column_separator, begining_str, comentary_char,
+                    hot_stars_filename=None, n_g_r_hot_stars = None, n_u_g_hot_stars = None, column_separator_hot_stars = None, begining_str_hot_stars = None, comentary_char_hot_stars = None):
     """
-    :param title: title of the graphic
+    :param title: plot title
     :param data_filename: name of the file, where the input data (the stars) are
     :param SP_filename: name of the file where the points of the main sequence are
     :param n_g_r_data: the number of the column (g-r) in the file 'data_filename'
@@ -232,35 +242,113 @@ def plot_graphic(title, data_filename, SP_filename="SP.txt", n_g_r_data=6, n_u_g
     :return: None, plot graphic u-g vs g-r, the main sequence, B3V line
     """
 
-    # get data
-    g_r_data, u_g_data = get_magnitudes(data_filename, n_g_r_data, n_u_g_data)
-    g_r_SP, u_g_SP = get_magnitudes(SP_filename, n_g_r_SP, n_u_g_SP)
+    g_r, u_g = get_magnitudes(filename, n_g_r, n_u_g, column_separator, begining_str, comentary_char)
+    plt.plot(g_r, u_g, '.', c='red', label='Stars')
 
-    # settings of the graph
+    if hot_stars_filename is not None:
+        if n_g_r_hot_stars is None:
+            n_g_r_hot_stars = n_g_r
+        if n_u_g_hot_stars is None:
+            n_u_g_hot_stars = n_u_g
+        if column_separator_hot_stars is None:
+            column_separator_hot_stars = column_separator
+        if begining_str_hot_stars is None:
+            begining_str_hot_stars = begining_str
+        if comentary_char_hot_stars is None:
+            comentary_char_hot_stars = comentary_char
+        g_r_hot_stars, u_g_hot_stars = get_magnitudes(hot_stars_filename, n_g_r_hot_stars, n_u_g_hot_stars, column_separator_hot_stars, begining_str_hot_stars, comentary_char_hot_stars)
+        plt.plot(g_r_hot_stars, u_g_hot_stars, '.', c='blue', label='Hot stars')
+
+    # plot B3V_line
+    m = min([x for x in g_r if x != None])
+    M = max([y for y in g_r if y != None])
+    x = np.linspace(m, M, 100)
+    plt.plot(x, B3V_line(x), c='orange', label='B3V stars')
+
+    # plot main sequence
+    plt.plot(Main_sequence_points().g_r_values, Main_sequence_points().u_g_values, c='black', label='Main sequence')
+
+    # graph settings
+    title(title)
     plt.xlabel('g-r')
     plt.ylabel('u-g')
     plt.gca().invert_yaxis()
-
-    # plot u-g vs g-r of the stars with our data
-    plt.plot(g_r_data, u_g_data, '.', c='red', label='Stars')
-    if hot_stars_filename != None:
-        g_r_hot_stars, u_g_hot_stars = get_magnitudes(hot_stars_filename, n_g_r_data, n_u_g_data)
-        plt.plot(g_r_hot_stars, u_g_hot_stars, '.', c='blue', label='Hot Stars')
-
-    # plot B3V_line
-    m = min([x for x in g_r_data if x != None])
-    M = max([y for y in g_r_data if y != None])
-    x = np.linspace(m, M, 100)
-    plt.plot(x, B3V_line(x), c='orange', label='B3V line')
-
-    # plot the main sequence
-    plt.plot(g_r_SP, u_g_SP, c='black', label='Main Sequence')
-
-    # add title and legends
-    title(title)
     plt.legend()
     plt.show()
 
+
+def save_plot(title, input_file, output_file, n_g_r, n_u_g, column_separator, begining_str, comentary_char, output_folder=None,
+                   input_file_hot_stars=None, n_g_r_hot_stars = None, n_u_g_hot_stars = None, column_separator_hot_stars = None, begining_str_hot_stars = None, comentary_char_hot_stars = None):
+    """
+    moi
+    :param titre: titre que l'on veut donner au graphique
+    :param input_file: nom du fichier qui contient les donnees d'entree correspondant a des etoiles
+    :param SP_filename: nom du fichier qui contient des coordonnees de points de la sequence principale
+    :param output_folder: nom du dossier dans lequel on travaille (la ou y a les catalogues d entree (sauf SP) et la ou on met le fichier de sortie)
+    :param n_g_r_data: numero de la colonne correspondant a g-r dans le fichier data_filename
+    :param n_u_g_data: numero de la colonne correspondant a u-g dans le fichier data_filename
+    :param n_g_r_SP: numero de la colonne correspondant a g-r dans le fichier SP_filename
+    :param n_u_g_SP: numero de la colonne correspondant a u-g dans le fichier SP_filename
+    :param input_file_hot_stars: facultatif, nom du fichier contenant uniquement les donnees des etoiles chaudes
+    dans data_filename pour afficher d'une autre couleur les points correspondant aux etoiles chaudes
+    :return: None, trace le graphique u-g vs g-r avec la sequence principale et la ligne B3V
+    """
+
+    if output_folder is not None:
+        output_folder_for_terminal = ""
+        for char in output_folder:
+            if char == " ":
+                output_folder_for_terminal += "\ "
+            elif char == "(":
+                output_folder_for_terminal += "\("
+            elif char == ")":
+                output_folder_for_terminal += "\)"
+            else:
+                output_folder_for_terminal += char
+        if not os.path.exists(output_folder):
+            os.system("mkdir " + output_folder_for_terminal)
+        input_file = output_folder + "/" + input_file
+        if input_file_hot_stars is not None:
+            input_file_hot_stars = output_folder + "/" + input_file_hot_stars
+        output_file = output_folder + "/" + output_file
+
+    g_r, u_g = get_magnitudes(input_file, n_g_r, n_u_g, column_separator, begining_str, comentary_char)
+    plt.plot(g_r, u_g, '.', c='red', label='Stars')
+
+    if input_file_hot_stars is not None:
+        if n_g_r_hot_stars is None:
+            n_g_r_hot_stars = n_g_r
+        if n_u_g_hot_stars is None:
+            n_u_g_hot_stars = n_u_g
+        if column_separator_hot_stars is None:
+            column_separator_hot_stars = column_separator
+        if begining_str_hot_stars is None:
+            begining_str_hot_stars = begining_str
+        if comentary_char_hot_stars is None:
+            comentary_char_hot_stars = comentary_char
+        g_r_hot_stars, u_g_hot_stars = get_magnitudes(input_file_hot_stars, n_g_r_hot_stars, n_u_g_hot_stars,
+                                                      column_separator_hot_stars, begining_str_hot_stars,
+                                                      comentary_char_hot_stars)
+        plt.plot(g_r_hot_stars, u_g_hot_stars, '.', c='blue', label='Hot stars')
+
+    # plot B3V_line
+    m = min([x for x in g_r if x != None])
+    M = max([y for y in g_r if y != None])
+    x = np.linspace(m, M, 100)
+    plt.plot(x, B3V_line(x), c='orange', label='B3V stars')
+
+    # plot main sequence
+    plt.plot(Main_sequence_points().g_r_values, Main_sequence_points().u_g_values, c='black', label='Main sequence')
+
+    # graph settings
+    title(title)
+    plt.xlabel('g-r')
+    plt.ylabel('u-g')
+    plt.gca().invert_yaxis()
+    plt.legend()
+    plt.savefig(output_file)
+
+#==========================j en suis a la===================
 
 def get_sky_picture(region_name, output_file, x_size, y_size, output_folder=None, coordinate_system="J2000",
                 survey="DSS2-red", ra="", dec=""):
@@ -364,71 +452,6 @@ def recup_catalogue(region_name, output_file, cone_size, output_folder=None, siz
         + "' -O " + output_file_for_terminal)
 
 
-def save_plot(output_file, input_file, titre, SP_filename="SP.txt", output_folder=None, n_g_r_data=6, n_u_g_data=5, n_g_r_SP=4, n_u_g_SP=3,
-              input_file_hot_stars=None):
-    """
-    moi
-    :param titre: titre que l'on veut donner au graphique
-    :param input_file: nom du fichier qui contient les donnees d'entree correspondant a des etoiles
-    :param SP_filename: nom du fichier qui contient des coordonnees de points de la sequence principale
-    :param output_folder: nom du dossier dans lequel on travaille (la ou y a les catalogues d entree (sauf SP) et la ou on met le fichier de sortie)
-    :param n_g_r_data: numero de la colonne correspondant a g-r dans le fichier data_filename
-    :param n_u_g_data: numero de la colonne correspondant a u-g dans le fichier data_filename
-    :param n_g_r_SP: numero de la colonne correspondant a g-r dans le fichier SP_filename
-    :param n_u_g_SP: numero de la colonne correspondant a u-g dans le fichier SP_filename
-    :param input_file_hot_stars: facultatif, nom du fichier contenant uniquement les donnees des etoiles chaudes
-    dans data_filename pour afficher d'une autre couleur les points correspondant aux etoiles chaudes
-    :return: None, trace le graphique u-g vs g-r avec la sequence principale et la ligne B3V
-    """
-
-    if output_folder is not None:
-        output_folder_for_terminal = ""
-        for char in output_folder:
-            if char == " ":
-                output_folder_for_terminal += "\ "
-            elif char == "(":
-                output_folder_for_terminal += "\("
-            elif char == ")":
-                output_folder_for_terminal += "\)"
-            else:
-                output_folder_for_terminal += char
-        if not os.path.exists(output_folder):
-            os.system("mkdir " + output_folder_for_terminal)
-        input_file = output_folder + "/" + input_file
-        if input_file_hot_stars is not None:
-            input_file_hot_stars = output_folder + "/" + input_file_hot_stars
-        output_file = output_folder + "/" + output_file
-
-    # recupere donnees
-    g_r_data, u_g_data = get_magnitudes(input_file, n_g_r_data, n_u_g_data)
-    g_r_SP, u_g_SP = get_magnitudes(SP_filename, n_g_r_SP, n_u_g_SP)
-
-    # parametre le graphique
-    plt.xlabel('g-r')
-    plt.ylabel('u-g')
-    plt.gca().invert_yaxis()
-
-    # trace u-g vs g-r avec nos donnees
-    plt.plot(g_r_data, u_g_data, '.', c='red', label='Etoiles')
-    if input_file_hot_stars != None:
-        g_r_hot_stars, u_g_hot_stars = get_magnitudes(input_file_hot_stars, n_g_r_data, n_u_g_data)
-        plt.plot(g_r_hot_stars, u_g_hot_stars, '.', c='blue', label='Etoiles chaudes')
-
-    # trace ligne B3V
-    m = min([x for x in g_r_data if x != None])
-    M = max([y for y in g_r_data if y != None])
-    x = np.linspace(m, M, 100)
-    plt.plot(x, B3V_line(x), c='orange', label='Ligne B3V')
-
-    # trace sequence principale
-    plt.plot(g_r_SP, u_g_SP, c='black', label='Séquence principale')
-
-    # met le titre et enregistre le tout
-    title(titre)
-    plt.legend()
-    plt.savefig(output_file)
-
-
 def analyser_region(region_name, cone_size):
 
     region_name_for_filenames = ""
@@ -465,11 +488,3 @@ def analyser_region(region_name, cone_size):
     os.chdir(output_folder)
     os.system("ds9 " + output_file_fits + " -regions " + output_file_reg + " -saveimage " + output_file_sky_picture + " -exit")
     os.chdir(oldpwd)
-
-
-"""
-plt.plot([i * 0.0001 for i in range(int(5 / 0.0001))], [main_sequence(i * 0.0001) for i in range(int(5 / 0.0001))], '.', c='red')
-plt.plot([i * -0.0001 for i in range(int(5 / 0.0001))], [main_sequence(i * -0.0001) for i in range(int(5 / 0.0001))], '.', c='red')
-plt.plot(Main_sequence_points().g_r_values, Main_sequence_points().u_g_values, c='black')
-plt.show()
-"""
